8f484d28493245174e32dc4c3aede297b910454e
Pedro Lima <143129211+Striffer@users.noreply.github.com>
2023-11-17 20:04:04 -0300

    Add files via upload

---
+++ 4/script.js
@@ -0,0 +1,4 @@
+document.getElementById('search_btn').addEventListener('click', () => {
+    document.getElementById('search_input').classList.toggle('search_input_open')
+    document.getElementById('search_input').classList.toggle('search_input_close')
+})
